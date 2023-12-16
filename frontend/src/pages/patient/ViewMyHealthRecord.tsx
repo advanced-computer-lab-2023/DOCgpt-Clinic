569d32bbe9ef147f8eecd6d17772999d94896f76
Rotanaahmeddd <rotanaabdalrahman110@gmail.com>
2023-12-16 16:02:02 +0200

    healthkhalas

--- frontend/src/pages/patient/ViewMyHealthRecord.tsx
+++ frontend/src/pages/patient/ViewMyHealthRecord.tsx
@@ -10,6 +10,9 @@ import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
 import ScaleIcon from '@mui/icons-material/Scale';
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 
 interface HealthRecord{
     MedicalHistory:{
@@ -63,6 +66,8 @@ function ViewMyHealthRecord(){
   const [submittedDocuments, setSubmittedDocuments] = useState<string[]>([]);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [isCommentsExpanded, setIsCommentsExpanded] = useState(false);
+  const [showNoHealthRecordMessage, setShowNoHealthRecordMessage] = useState<boolean>(false);
+
     useEffect(() => {
         async function fetchHealthRecord() {
             try {
@@ -87,6 +92,13 @@ function ViewMyHealthRecord(){
         fetchHealthRecord();
       }, []); // An empty dependency array means this effect runs once on component mount.
 
+      useEffect(() => {
+        if (!healthRecord) {
+          setShowNoHealthRecordMessage(true);
+        } else {
+          setShowNoHealthRecordMessage(false);
+        }
+      }, [healthRecord]);
       const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const files = e.target.files;
         if (files) {
@@ -169,7 +181,7 @@ function ViewMyHealthRecord(){
         >
           <PatientBar />
           
-          {healthRecord && (
+          {healthRecord ? (
             <Container>
               {/* Vital Signs Section */}
               <div
@@ -180,7 +192,9 @@ function ViewMyHealthRecord(){
     justifyContent: 'center',
 
   }}
+
 >
+  
   <AddCircleOutlineIcon
     style={{  color: 'red', fontSize: '6rem', margin: '20px 0' }}
   />
@@ -213,20 +227,20 @@ function ViewMyHealthRecord(){
   <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
     <OpacityIcon style={{ color: 'red', marginRight: '10px' }} /> {/* Add the icon here */}
     <Typography style={{ marginRight: '120px' }}>
-      Blood Pressure: {`${healthRecord.VitalSigns.BloodPressure || 'Null'}`}
+      Blood Pressure: {`${healthRecord.VitalSigns.BloodPressure || 'None'}`}
     </Typography>
     <MonitorHeartIcon style={{ color: 'red', marginLeft: '10px' }} />
     <Typography style={{ marginRight: '190px' }}>
-      Heart Rate: {`${healthRecord.VitalSigns.HeartRate || 'Null'}`}
+      Heart Rate: {`${healthRecord.VitalSigns.HeartRate || 'None'}`}
     </Typography>
     {/* Add the icon here */}
     <AccessibilityNewIcon style={{ marginRight: '8px', color: 'red' }} /> {/* Add this line */}
     <Typography style={{ marginRight: '80px' }}>
-      Height: {`${healthRecord.VitalSigns.Height || 'Null'}`}
+      Height: {`${healthRecord.VitalSigns.Height || 'None'}`}
     </Typography>
     <ScaleIcon style={{ color: 'red' }} /> {/* Add this line */}
     <Typography style={{ marginLeft: '20px' }}>
-      Weight: {`${healthRecord.VitalSigns.Weight || 'Null'}`}
+      Weight: {`${healthRecord.VitalSigns.Weight || 'None'}`}
     </Typography>
   </div>
 </CardContent>
@@ -395,6 +409,7 @@ function ViewMyHealthRecord(){
           color: "#fff", // Text color
           transition: "background-color 0.3s ease", // Smooth transition on hover
           marginTop: "10px", // Adjust the margin-top here
+          marginLeft:"56px"
         }}
         onMouseOver={(e) => (e.currentTarget.style.backgroundColor = "#4285f4")} // Hover style
         onMouseOut={(e) => (e.currentTarget.style.backgroundColor = "grey")} // Revert to grey on mouse out
@@ -438,10 +453,11 @@ function ViewMyHealthRecord(){
         variant="contained"
         onClick={handleSubmit("Laboratory", "XRays")}
         style={{
-          backgroundColor: "#grey", // Grey background color
+          backgroundColor: "grey", // Grey background color
           color: "#fff", // Text color
           transition: "background-color 0.3s ease", // Smooth transition on hover
           marginTop: "10px", // Adjust the margin-top here
+          marginLeft:"56px"
         }}
         onMouseOver={(e) => (e.currentTarget.style.backgroundColor = "#4285f4")} // Hover style
         onMouseOut={(e) => (e.currentTarget.style.backgroundColor = "grey")} // Revert to grey on mouse out
@@ -494,6 +510,8 @@ function ViewMyHealthRecord(){
           color: "#fff", // Text color
           transition: "background-color 0.3s ease", // Smooth transition on hover
           marginTop: "10px", // Adjust the margin-top here
+          marginLeft:"56px"
+
         }}
         onMouseOver={(e) => (e.currentTarget.style.backgroundColor = "#4285f4")} // Hover style
         onMouseOut={(e) => (e.currentTarget.style.backgroundColor = "grey")} // Revert to grey on mouse out
@@ -545,6 +563,8 @@ function ViewMyHealthRecord(){
           color: "#fff", // Text color
           transition: "background-color 0.3s ease", // Smooth transition on hover
           marginTop: "10px", // Adjust the margin-top here
+          marginLeft:"56px"
+
         }}
         onMouseOver={(e) => (e.currentTarget.style.backgroundColor = "#4285f4")} // Hover style
         onMouseOut={(e) => (e.currentTarget.style.backgroundColor = "grey")} // Revert to grey on mouse out
@@ -586,7 +606,300 @@ function ViewMyHealthRecord(){
 
 
               </Container>
-          )}
+         ) : (
+
+
+
+
+          <Container>
+          {/* Vital Signs Section with all values set to null */}
+          <div
+  style={{
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  }}
+
+>
+  
+  <AddCircleOutlineIcon
+    style={{  color: 'red', fontSize: '6rem', margin: '20px 0' }}
+  />
+</div>
+
+                  <div style={{ width: "100%", height: "2rem" }}></div>
+<Card
+  style={{
+    width: '97.4%',
+    height: '100px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginBottom: '10px',
+    marginLeft: '28px'
+  }}
+>
+  <Paper
+    style={{ backgroundColor: 'white', padding: '10px', width: '1200px' }}
+    elevation={3}
+  >
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <Typography variant="h6" style={{ color: 'black' }}>
+        Vital Signs
+      </Typography>
+    </div>
+  </Paper>
+  
+  <CardContent style={{ flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <OpacityIcon style={{ color: 'red', marginRight: '10px' }} />
+      <Typography style={{ marginRight: '120px' }}>
+        Blood Pressure: None
+      </Typography>
+      <MonitorHeartIcon style={{ color: 'red', marginLeft: '10px' }} />
+      <Typography style={{ marginRight: '190px' }}>
+        Heart Rate: None
+      </Typography>
+      <AccessibilityNewIcon style={{ marginRight: '8px', color: 'red' }} />
+      <Typography style={{ marginRight: '80px' }}>
+        Height: None
+      </Typography>
+      <ScaleIcon style={{ color: 'red' }} />
+      <Typography style={{ marginLeft: '20px' }}>
+        Weight: None
+      </Typography>
+    </div>
+  </CardContent>
+</Card>
+
+            {/* Medical History Section with all values set to null */}
+<Card
+    style={{
+      height: '250px',
+      width: "48%",
+      display: "flex",
+      flexDirection: "column",
+      marginLeft: '20px'
+    }}
+  >
+    <Paper
+      style={{ backgroundColor: "white", padding: "10px" }}
+      elevation={3}
+    >
+      <Typography variant="h6" style={{ color: "black" }}>
+        Medical History
+      </Typography>
+    </Paper>
+    <CardContent style={{ flex: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography>Allergies:</Typography>
+          <Typography>No data provided</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography>Past Medical Conditions:</Typography>
+          <Typography>No data provided</Typography>
+        </Grid>
+      </Grid>
+    </CardContent>
+  </Card>
+
+
+            
+           {/* Medication List Section with all values set to null */}
+<Card
+  style={{
+    height: '250px',
+    width: "48%",
+    display: "flex",
+    flexDirection: "column",
+    marginLeft: '20px', // Add margin to create space
+    marginTop: '10px',
+  }}
+>
+  <Paper
+    style={{ backgroundColor: "white", padding: "10px" }}
+    elevation={3}
+  >
+    <Typography variant="h6" style={{ color: "black" }}>
+      Medication List
+    </Typography>
+  </Paper>
+  <CardContent style={{ flex: 1 }}>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Typography>Current Medications:</Typography>
+        <Typography>No data provided</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography>Past Medications:</Typography>
+        <Typography>No data provided</Typography>
+      </Grid>
+    </Grid>
+  </CardContent>
+</Card>
+
+    
+            {/* Upload Documents Section with all values set to null */}
+               {/* Upload Documents Section */}
+          <Card
+  style={{
+    height: '510px',
+    width: "48%",
+    display: "flex",
+    flexDirection: "column",
+    marginLeft: '635px', // Add margin to create space
+    marginTop: '-510px',
+  }}
+>
+  <Paper
+    style={{ backgroundColor: "white", padding: "10px" }}
+    elevation={3}
+  >
+    <Typography variant="h6" style={{ color: "black" }}>
+      Upload Documents
+    </Typography>
+  </Paper>
+  <CardContent style={{ flex: 1 }}>
+    {/* BloodTests Upload Section */}
+    <Grid item xs={6}>
+      <Typography>BloodTests: </Typography>
+     
+    </Grid>
+    <div style={{ marginTop: '30px' }}></div> 
+    <Grid item xs={6}>
+      <input
+        type="file"
+        accept=".pdf, .jpg, .jpeg, .png"
+        onChange={handleFileChange}
+      />
+      <Button
+        variant="contained"
+        onClick={handleSubmit("Laboratory", "BloodTests")}
+        style={{
+          backgroundColor: "grey", // Grey background color
+          color: "#fff", // Text color
+          transition: "background-color 0.3s ease", // Smooth transition on hover
+          marginTop: "10px", // Adjust the margin-top here
+        }}
+        onMouseOver={(e) => (e.currentTarget.style.backgroundColor = "#4285f4")} // Hover style
+        onMouseOut={(e) => (e.currentTarget.style.backgroundColor = "grey")} // Revert to grey on mouse out
+      >
+        Submit Document
+      </Button>
+    </Grid>
+
+    {/* XRays Upload Section */}
+    <Grid item xs={6}>
+      <Typography>XRays: </Typography>
+    
+    </Grid>
+    <div style={{ marginTop: '30px' }}></div> 
+    <Grid item xs={6}>
+      <input
+        type="file"
+        accept=".pdf, .jpg, .jpeg, .png"
+        onChange={handleFileChange}
+      />
+      <Button
+        variant="contained"
+        onClick={handleSubmit("Laboratory", "XRays")}
+        style={{
+          backgroundColor: "grey", // Grey background color
+          color: "#fff", // Text color
+          transition: "background-color 0.3s ease", // Smooth transition on hover
+          marginTop: "10px", // Adjust the margin-top here
+        }}
+        onMouseOver={(e) => (e.currentTarget.style.backgroundColor = "#4285f4")} // Hover style
+        onMouseOut={(e) => (e.currentTarget.style.backgroundColor = "grey")} // Revert to grey on mouse out
+      >
+        Submit Document
+      </Button>
+    </Grid>
+
+    {/* Medication List Documents Upload Section */}
+    <Grid item xs={6}>
+      <Typography>Current Medications: </Typography>
+     
+    
+    </Grid>
+ <div style={{ marginTop: '30px' }}></div> 
+    <Grid item xs={6}>
+      <input
+        type="file"
+        accept=".pdf, .jpg, .jpeg, .png"
+        onChange={handleFileChange}
+      />
+     
+      <Button
+        variant="contained"
+        onClick={handleSubmit("MedicationList", "CurrentMedications")}
+        style={{
+          backgroundColor: "grey", // Grey background color
+          color: "#fff", // Text color
+          transition: "background-color 0.3s ease", // Smooth transition on hover
+          marginTop: "10px", // Adjust the margin-top here
+        
+        }}
+        onMouseOver={(e) => (e.currentTarget.style.backgroundColor = "#4285f4")} // Hover style
+        onMouseOut={(e) => (e.currentTarget.style.backgroundColor = "grey")} // Revert to grey on mouse out
+      >
+        Submit Document
+      </Button>
+    </Grid>
+  
+    <Grid item xs={6}>
+      <Typography>Past Medications: </Typography>
+      
+      <div style={{ marginTop: '30px' }}></div> 
+
+     
+    </Grid>
+    <Grid item xs={6}>
+      <input
+        type="file"
+        accept=".pdf, .jpg, .jpeg, .png"
+        onChange={handleFileChange}
+      />
+      <Button
+        variant="contained"
+        onClick={handleSubmit("MedicationList", "PastMedications")}
+        style={{
+          backgroundColor: "grey", // Grey background color
+          color: "#fff", // Text color
+          transition: "background-color 0.3s ease", // Smooth transition on hover
+          marginTop: "10px", // Adjust the margin-top here
+        }}
+        onMouseOver={(e) => (e.currentTarget.style.backgroundColor = "#4285f4")} // Hover style
+        onMouseOut={(e) => (e.currentTarget.style.backgroundColor = "grey")} // Revert to grey on mouse out
+      >
+        Submit Document
+      </Button>
+    </Grid>
+  </CardContent>
+</Card>
+    
+            {/* General Comments Section with all values set to null */}
+            <Accordion style={{ width: '98%', margin: '0 auto', marginTop: '20px', marginBottom: '40px', marginLeft: '20px' }}>
+              <AccordionSummary
+                aria-controls="general-comments-content"
+                id="general-comments-header"
+                expandIcon={<ExpandMoreIcon />}
+              >
+                <Typography variant="h6" style={{ fontWeight: 'bold', margin: 'auto' }}>General Comments</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Card style={{ width: '98%', height: '120px' }}>
+                  <CardContent>
+                  <Typography >No General Comments Yet</Typography>
+                  </CardContent>
+                </Card>
+              </AccordionDetails>
+            </Accordion>
+          </Container>
+        )}
         </div>
       );
       
