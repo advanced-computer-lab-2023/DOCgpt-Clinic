37821ab00ed44676599ac13bb423bf567cce6012
farah-amr <farah.amr28@gmail.com>
2023-12-17 02:53:46 +0200

    dhdh

--- backend/routes/prescription.ts
+++ backend/routes/prescription.ts
@@ -36,7 +36,7 @@ router.post('/addToCart',  addPrescriptionToCart);
 router.post('/changeStatus',  changeStatus);
 router.delete('/deleteMedPresc/:prescriptionId', deleteMedPresc);
 router.put('updatePrescMed/:prescriptionId', updatePrescriptionMed);
-
+updateMedicineInPrescription
 
 
 router.post('/checkmedicineexists',checkifexists);
